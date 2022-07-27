0f00c18eb64eb349fdaa82fdb012d1c64a56f5da
LeeHyeonHo-127 <swethom1@naver.com>
2022-07-28 01:01:27 +0900

    [ADD/현호] #001 백준문제 1330

---
+++ helloSwift/baekjoon1330.swift
@@ -0,0 +1,20 @@
+import Foundation
+
+
+
+var arr = readLine()!.split(separator: " ").map{Int(String($0))!}
+let a = arr[0]
+let b = arr[1]
+
+if(a>b){
+    print(">")
+}
+else if(a<b)
+{
+    print("<")
+}
+else
+{
+    print("==")
+}
+
