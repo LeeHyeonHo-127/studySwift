a830f57af6a6b27e812aea633359013b57ad98c9
LeeHyeonHo-127 <84439622+LeeHyeonHo-127@users.noreply.github.com>
2022-08-15 22:50:11 +0900

    [ADD/현호] #001 백준문제 1157 문자열

---
+++ helloSwift/baekjoon1157.swift
@@ -0,0 +1,52 @@
+import Foundation
+
+
+//백준문제 1157
+/*
+ 문제
+ 알파벳 대소문자로 된 단어가 주어지면, 이 단어에서 가장 많이 사용된 알파벳이 무엇인지 알아내는 프로그램을 작성하시오. 단, 대문자와 소문자를 구분하지 않는다.
+
+ 입력
+ 첫째 줄에 알파벳 대소문자로 이루어진 단어가 주어진다. 주어지는 단어의 길이는 1,000,000을 넘지 않는다.
+
+ 출력
+ 첫째 줄에 이 단어에서 가장 많이 사용된 알파벳을 대문자로 출력한다. 단, 가장 많이 사용된 알파벳이 여러 개 존재하는 경우에는 ?를 출력한다.
+ */
+
+import Foundation
+
+var str = readLine()!.map{String($0)}
+var alphabet: [Int] = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+var check: Int = 0
+var highestIndex = 0
+
+
+for i in 0..<str.count{
+    let intTemp: Int = Int(Unicode.Scalar(str[i])!.value)
+    if(intTemp >= 97){
+        let temp = intTemp - 97
+        alphabet[temp] += 1
+    }
+    else{
+        let temp = intTemp - 65
+        alphabet[temp] += 1
+    }
+}
+
+var highest = Int(alphabet.max()!)
+
+for i in 0..<alphabet.count{
+    if(highest == alphabet[i]){
+        highestIndex = i
+        check += 1
+    }
+}
+
+if check >= 2 {
+    print("?")
+}
+else{
+    let unicode: String = String(Unicode.Scalar(65+highestIndex)!)
+    print(unicode)
+}
+
