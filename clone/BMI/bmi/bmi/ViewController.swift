3c2b3e726a75b0d308140f981cbf6d25a7ebe0ab
LeeHyeonHo-127 <84439622+LeeHyeonHo-127@users.noreply.github.com>
2022-08-16 16:37:09 +0900

    bmi clone 시작

---
+++ clone/BMI/bmi/bmi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  bmi
+//
+//  Created by 이현호 on 2022/08/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
