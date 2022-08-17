7bd275987ef9b4846f5a4a8cb79772796d68b4b3
LeeHyeonHo-127 <84439622+LeeHyeonHo-127@users.noreply.github.com>
2022-08-17 15:13:54 +0900

    [ADD/현호] #002 bmi 앱 개발_segmentController 추가

--- clone/BMI/bmi/bmi/ViewController.swift
+++ clone/BMI/bmi/bmi/ViewController.swift
@@ -11,19 +11,33 @@ class ViewController: UIViewController {
 
     @IBOutlet var txtHeight: UITextField!
     @IBOutlet var txtWeight: UITextField!
+    @IBOutlet var bmiImage: UIImageView!
     @IBOutlet var lblResult: UILabel!
-    
+    var sex: Int = 1
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func sexController(_ sender: UISegmentedControl) {
+        if sender.selectedSegmentIndex == 0{
+            bmiImage.image = UIImage(named: "bmiImage2") //image 변환
+            sex = 1
+        }else{
+            bmiImage.image = UIImage(named: "bmi_woman")
+            sex = 2
+        }
+    }
+    
+    
     @IBAction func calcBMI(_ sender: UIButton) {
         if(txtHeight.text == "" || txtWeight.text == ""){
             lblResult.textColor = UIColor(displayP3Red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
             lblResult.text = "키와 체중을 모두 입력해주세요."
             return
         }
-        else{
+        else if(sex == 1){
             let height = Double(txtHeight.text!)! // textfield.text => <String!> 형
             let weight = Double(txtWeight.text!)!
             print(height,weight)
@@ -33,21 +47,53 @@ class ViewController: UIViewController {
             var body = ""
             var color = UIColor.white
             
-            if bmi >= 40 {
+            if bmi >= 30 {
                 body = "3단계 비만"
                 color = UIColor(displayP3Red: 1.0, green: 0.0, blue: 0.0, alpha: 1.0)
-            } else if bmi >= 30 && bmi < 40 {
-                body = "2단계 비만"
-                color = UIColor(displayP3Red: 0.7, green: 0.0, blue: 0.0, alpha: 1.0)
             } else if bmi >= 25 && bmi < 30 {
+                body = "2단계 비만"
+                color = UIColor(displayP3Red: 1.0, green: 0.5, blue: 0.0, alpha: 1.0)
+            } else if bmi >= 23 && bmi < 25 {
                 body = "1단계 비만"
-                color = UIColor(displayP3Red: 0.4, green: 0.0, blue: 0.0, alpha: 1.0)
-            } else if bmi >= 18.5 && bmi < 25 {
+                color = UIColor(displayP3Red: 1.0, green: 1.0, blue: 0.0, alpha: 1.0)
+            } else if bmi >= 18.5 && bmi < 23 {
                 body = "정상"
-                color = UIColor(displayP3Red: 0.0, green: 0.0, blue: 1.0, alpha: 1.0)
+                color = UIColor(displayP3Red: 0.0, green: 1.0, blue: 0.0, alpha: 1.0)
             } else {
                 body = "저체중"
+                color = UIColor(displayP3Red: 0.0, green: 0.0, blue: 1.0, alpha: 1.0)
+            }
+            print("BMI:\(shortenedBmi), 판정:\(body)")
+            lblResult.textColor = color
+            lblResult.clipsToBounds = true
+            lblResult.layer.cornerRadius = 7
+            lblResult.text = "BMI:\(shortenedBmi)      판정:\(body)"
+            
+        }else if(sex == 0){
+            let height = Double(txtHeight.text!)! // textfield.text => <String!> 형
+            let weight = Double(txtWeight.text!)!
+            print(height,weight)
+            
+            let bmi = weight/(height*height*0.0001)
+            let shortenedBmi = String(format: "%.1f", bmi)
+            var body = ""
+            var color = UIColor.white
+            
+            if bmi >= 35 {
+                body = "3단계 비만"
+                color = UIColor(displayP3Red: 1.0, green: 0.0, blue: 0.0, alpha: 1.0)
+            } else if bmi >= 30 && bmi < 35 {
+                body = "2단계 비만"
+                color = UIColor(displayP3Red: 1.0, green: 0.5, blue: 0.0, alpha: 1.0)
+            } else if bmi >= 25 && bmi < 30 {
+                body = "1단계 비만"
+                color = UIColor(displayP3Red: 1.0, green: 1.0, blue: 0.0, alpha: 1.0)
+            } else if bmi >= 18.5 && bmi < 25 {
+                body = "정상"
                 color = UIColor(displayP3Red: 0.0, green: 1.0, blue: 0.0, alpha: 1.0)
+            } else {
+                body = "저체중"
+                color = UIColor(displayP3Red: 0.0, green: 0.0, blue: 1.0, alpha: 1.0)
             }
             print("BMI:\(shortenedBmi), 판정:\(body)")
             lblResult.textColor = color
@@ -63,3 +109,7 @@ class ViewController: UIViewController {
 
 }
 
+//여자 BMI 계산 공식
+//https://www.everydayhealth.com/diet-nutrition/bmi/bmi-adults-yours-healthy-not-how-can-you-lose-weight/
+//남자 BMI 계산 공식
+//https://nutrition.health.gov.lk/english/body-mass-index-bmi/
