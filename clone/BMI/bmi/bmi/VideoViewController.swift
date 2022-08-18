d001c6e3418e1cc892a6ceac018c426cc9835507
LeeHyeonHo-127 <84439622+LeeHyeonHo-127@users.noreply.github.com>
2022-08-18 17:17:22 +0900

    [ADD/현호] #004 bmi 앱 개발_영상 재생 화면 추가

---
+++ clone/BMI/bmi/bmi/VideoViewController.swift
@@ -0,0 +1,40 @@
+//
+//  VideoViewController.swift
+//  bmi
+//
+//  Created by 이현호 on 2022/08/18.
+//
+
+import UIKit
+import AVKit
+
+class VideoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func playVideo(_ sender: UIButton) {
+        let file: String? = Bundle.main.path(forResource: "whatIsBMI", ofType: "mp4")
+        let url = NSURL(fileURLWithPath: file!)
+        let playerController = AVPlayerViewController()
+        let player = AVPlayer(url: url as URL)
+        playerController.player = player
+        self.present(playerController, animated: true)
+        player.play()
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- clone/BMI/bmi/bmi/ViewController.swift
+++ clone/BMI/bmi/bmi/ViewController.swift
@@ -21,11 +21,11 @@ class ViewController: UIViewController {
     }
     
     @IBAction func sexController(_ sender: UISegmentedControl) {
-        if sender.selectedSegmentIndex == 0{
-            bmiImage.image = UIImage(named: "bmiImage2") //image 변환
+        if sender.selectedSegmentIndex == 1{
+            bmiImage.image = UIImage(named: "womanBMIImage") //image 변환
             sex = 1
         }else{
-            bmiImage.image = UIImage(named: "bmi_woman")
+            bmiImage.image = UIImage(named: "manBMIImage")
             sex = 2
         }
     }
@@ -37,7 +37,7 @@ class ViewController: UIViewController {
             lblResult.text = "키와 체중을 모두 입력해주세요."
             return
         }
-        else if(sex == 1){
+        else{
             let height = Double(txtHeight.text!)! // textfield.text => <String!> 형
             let weight = Double(txtWeight.text!)!
             print(height,weight)
@@ -69,38 +69,6 @@ class ViewController: UIViewController {
             lblResult.layer.cornerRadius = 7
             lblResult.text = "BMI:\(shortenedBmi)      판정:\(body)"
             
-        }else if(sex == 0){
-            let height = Double(txtHeight.text!)! // textfield.text => <String!> 형
-            let weight = Double(txtWeight.text!)!
-            print(height,weight)
-            
-            let bmi = weight/(height*height*0.0001)
-            let shortenedBmi = String(format: "%.1f", bmi)
-            var body = ""
-            var color = UIColor.white
-            
-            if bmi >= 35 {
-                body = "3단계 비만"
-                color = UIColor(displayP3Red: 1.0, green: 0.0, blue: 0.0, alpha: 1.0)
-            } else if bmi >= 30 && bmi < 35 {
-                body = "2단계 비만"
-                color = UIColor(displayP3Red: 1.0, green: 0.5, blue: 0.0, alpha: 1.0)
-            } else if bmi >= 25 && bmi < 30 {
-                body = "1단계 비만"
-                color = UIColor(displayP3Red: 1.0, green: 1.0, blue: 0.0, alpha: 1.0)
-            } else if bmi >= 18.5 && bmi < 25 {
-                body = "정상"
-                color = UIColor(displayP3Red: 0.0, green: 1.0, blue: 0.0, alpha: 1.0)
-            } else {
-                body = "저체중"
-                color = UIColor(displayP3Red: 0.0, green: 0.0, blue: 1.0, alpha: 1.0)
-            }
-            print("BMI:\(shortenedBmi), 판정:\(body)")
-            lblResult.textColor = color
-            lblResult.clipsToBounds = true
-            lblResult.layer.cornerRadius = 7
-            lblResult.text = "BMI:\(shortenedBmi)      판정:\(body)"
-            
         }
         
         
@@ -109,7 +77,3 @@ class ViewController: UIViewController {
 
 }
 
-//여자 BMI 계산 공식
-//https://www.everydayhealth.com/diet-nutrition/bmi/bmi-adults-yours-healthy-not-how-can-you-lose-weight/
-//남자 BMI 계산 공식
-//https://nutrition.health.gov.lk/english/body-mass-index-bmi/
