40665bacafe135e4e2ae6884274dae01b37970b1
LeeHyeonHo-127 <84439622+LeeHyeonHo-127@users.noreply.github.com>
2022-09-03 21:28:00 +0900

    [ADD/현호] #001 백준문제 10737 기본수학

---
+++ helloSwift/baekjoon10737.swift
@@ -0,0 +1,15 @@
+//백준 10757
+/*문제
+ 문제
+ 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+
+ 입력
+ 첫째 줄에 A와 B가 주어진다. (0 < A,B < 1010000)
+
+ 출력
+ 첫째 줄에 A+B를 출력한다.*/
+import Foundation
+
+var a = readLine()!.split(separator: " ")
+var firstArray = a[0].split(separator:"")
+print(firstArray[2])
